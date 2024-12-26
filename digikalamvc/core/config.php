9490cf388ff29363e691b0a593c959e01e0bc06c
Mohammad Sarraf <155878008+msarraf83@users.noreply.github.com>
2024-12-26 14:59:05 +0330

    Delete digikalamvc directory

--- digikalamvc/core/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('URL','http://127.0.0.1:8080/digikalamvc/');
-
-?>
--- digikalamvc/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require('core/config.php');
-require('core/app.php');
-require('core/controller.php');
-
-new app;
-
-?>
